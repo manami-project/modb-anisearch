2f79beb5d5d8c9768fc000b809290295f7873df0
manami-project <13179375+manami-project@users.noreply.github.com>
2025-01-05 21:49:44 +0100

    Added tests to verify that all test resources are part of the update resources list

--- src/test/kotlin/io/github/manamiproject/modb/anisearch/UpdateTestResources.kt
+++ src/test/kotlin/io/github/manamiproject/modb/anisearch/UpdateTestResources.kt
@@ -1,111 +1,132 @@
 package io.github.manamiproject.modb.anisearch
 
 import io.github.manamiproject.modb.core.coroutines.CoroutineManager.runCoroutine
+import io.github.manamiproject.modb.core.extensions.fileSuffix
 import io.github.manamiproject.modb.core.extensions.writeToFile
+import io.github.manamiproject.modb.core.random
 import io.github.manamiproject.modb.test.testResource
+import kotlinx.coroutines.delay
+import org.assertj.core.api.Assertions.assertThat
+import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import kotlin.io.path.isRegularFile
+import kotlin.test.Test
 
-internal fun main() {
+
+private val mainConfigFiles = mapOf(
+    "file_converter_tests/anime_season/season/01.html" to "14628",
+    "file_converter_tests/anime_season/season/02.html" to "14967",
+    "file_converter_tests/anime_season/season/03.html" to "16251",
+    "file_converter_tests/anime_season/season/04.html" to "14508",
+    "file_converter_tests/anime_season/season/05.html" to "15264",
+    "file_converter_tests/anime_season/season/06.html" to "14935",
+    "file_converter_tests/anime_season/season/07.html" to "14816",
+    "file_converter_tests/anime_season/season/08.html" to "16401",
+    "file_converter_tests/anime_season/season/09.html" to "16401",
+    "file_converter_tests/anime_season/season/10.html" to "15606",
+    "file_converter_tests/anime_season/season/11.html" to "15042",
+    "file_converter_tests/anime_season/season/12.html" to "14484",
+    "file_converter_tests/anime_season/season/unknown.html" to "16275",
+    "file_converter_tests/anime_season/season/year_only.html" to "17467",
+
+    "file_converter_tests/anime_season/year_of_premiere/1958-11.html" to "5976",
+    "file_converter_tests/anime_season/year_of_premiere/1991.html" to "168",
+    "file_converter_tests/anime_season/year_of_premiere/2021-08-06.html" to "15890",
+    "file_converter_tests/anime_season/year_of_premiere/unknown.html" to "16275",
+
+    "file_converter_tests/duration/134_minutes.html" to "602",
+    "file_converter_tests/duration/1_hour.html" to "6247",
+    "file_converter_tests/duration/1_minute.html" to "11051",
+    "file_converter_tests/duration/24_minutes_per_episode.html" to "14844",
+    "file_converter_tests/duration/2_hours.html" to "6889",
+    "file_converter_tests/duration/63_minutes_by_6_episodes.html" to "7192",
+    "file_converter_tests/duration/70_minutes.html" to "7163",
+    "file_converter_tests/duration/episodes_and_duration_unknown.html" to "16711",
+    "file_converter_tests/duration/episodes_known_duration_unknown.html" to "17039",
+
+    "file_converter_tests/episodes/1.html" to "9981",
+    "file_converter_tests/episodes/10.html" to "3135",
+    "file_converter_tests/episodes/100.html" to "4138",
+    "file_converter_tests/episodes/1818.html" to "5801",
+    "file_converter_tests/episodes/type-is-double.html" to "16804",
+    "file_converter_tests/episodes/unknown.html" to "16578",
+
+    "file_converter_tests/picture_and_thumbnail/neither_picture_nor_thumbnail.html" to "15237",
+    "file_converter_tests/picture_and_thumbnail/picture_and_thumbnail_available.html" to "3633",
+
+    "file_converter_tests/sources/3633.html" to "3633",
+
+    "file_converter_tests/status/aborted.html" to "12433",
+    "file_converter_tests/status/completed.html" to "3633",
+    "file_converter_tests/status/completed_in_japan_upcoming_elsewhere.html" to "6222",
+    "file_converter_tests/status/no_status.html" to "14494",
+    "file_converter_tests/status/on_hold.html" to "16925",
+    "file_converter_tests/status/ongoing.html" to "1721",
+    "file_converter_tests/status/upcoming.html" to "12224",
+
+    "file_converter_tests/synonyms/multiple_synonyms.html" to "1958",
+    "file_converter_tests/synonyms/no_synonyms.html" to "16260",
+    "file_converter_tests/synonyms/romanji_alteration.html" to "13631",
+    "file_converter_tests/synonyms/single_synonym.html" to "14456",
+
+    "file_converter_tests/synonyms/hidden_synonyms_11197.html" to "11197",
+    "file_converter_tests/synonyms/hidden_synonyms_8724.html" to "8724",
+    "file_converter_tests/synonyms/hidden_synonyms_and_named_parts.html" to "8093",
+    "file_converter_tests/synonyms/italic.html" to "17015",
+    "file_converter_tests/synonyms/multiple_synonyms.html" to "1958",
+    "file_converter_tests/synonyms/no_synonyms.html" to "16260",
+    "file_converter_tests/synonyms/romanji_alteration.html" to "13631",
+    "file_converter_tests/synonyms/single_synonym.html" to "14456",
+    "file_converter_tests/synonyms/synonyms_contain_named_parts.html" to "15599",
+
+    "file_converter_tests/tags/multiple_tags.html" to "15073",
+    "file_converter_tests/tags/no_tags.html" to "17467",
+    "file_converter_tests/tags/one_tag.html" to "613",
+
+    "file_converter_tests/title/special_chars.html" to "15159",
+    "file_converter_tests/title/title_not_set_in_jsonld.html" to "4410",
+
+    "file_converter_tests/type/bonus.html" to "10454",
+    "file_converter_tests/type/cm.html" to "12290",
+    "file_converter_tests/type/movie.html" to "9981",
+    "file_converter_tests/type/music-video.html" to "9830",
+    "file_converter_tests/type/other.html" to "16289",
+    "file_converter_tests/type/ova.html" to "3627",
+    "file_converter_tests/type/tv-series.html" to "4946",
+    "file_converter_tests/type/tv-special.html" to "13250",
+    "file_converter_tests/type/unknown.html" to "17467",
+    "file_converter_tests/type/web.html" to "14935",
+
+    "file_converter_tests/related_anime/multiple_related_anime_main.html" to "4942",
+    "file_converter_tests/related_anime/no_related_anime_but_adaption_main.html" to "14844",
+    "file_converter_tests/related_anime/no_related_anime_main.html" to "10941",
+    "file_converter_tests/related_anime/related_anime_file_missing_main.html" to "4942",
+    "file_converter_tests/related_anime/single_related_anime_main.html" to "16777",
+)
+
+private val relationsConfigFiles = mapOf(
+    "file_converter_tests/related_anime/multiple_related_anime.html" to "4942",
+    "file_converter_tests/related_anime/no_related_anime.html" to "10941",
+    "file_converter_tests/related_anime/no_related_anime_but_adaption.html" to "14844",
+    "file_converter_tests/related_anime/single_related_anime.html" to "16777",
+)
+
+internal fun main(): Unit = runCoroutine {
     val downloader = AnisearchDownloader(AnisearchConfig)
     val relationsDownloader = AnisearchDownloader(AnisearchRelationsConfig)
-    
-    runCoroutine {
-        downloader.download("14628").writeToFile(resourceFile("file_converter_tests/anime_season/season/01.html"))
-        downloader.download("14967").writeToFile(resourceFile("file_converter_tests/anime_season/season/02.html"))
-        downloader.download("16251").writeToFile(resourceFile("file_converter_tests/anime_season/season/03.html"))
-        downloader.download("14508").writeToFile(resourceFile("file_converter_tests/anime_season/season/04.html"))
-        downloader.download("15264").writeToFile(resourceFile("file_converter_tests/anime_season/season/05.html"))
-        downloader.download("14935").writeToFile(resourceFile("file_converter_tests/anime_season/season/06.html"))
-        downloader.download("14816").writeToFile(resourceFile("file_converter_tests/anime_season/season/07.html"))
-        downloader.download("16401").writeToFile(resourceFile("file_converter_tests/anime_season/season/08.html"))
-        downloader.download("16401").writeToFile(resourceFile("file_converter_tests/anime_season/season/09.html"))
-        downloader.download("15606").writeToFile(resourceFile("file_converter_tests/anime_season/season/10.html"))
-        downloader.download("15042").writeToFile(resourceFile("file_converter_tests/anime_season/season/11.html"))
-        downloader.download("14484").writeToFile(resourceFile("file_converter_tests/anime_season/season/12.html"))
-        downloader.download("16275").writeToFile(resourceFile("file_converter_tests/anime_season/season/unknown.html"))
-        downloader.download("17467").writeToFile(resourceFile("file_converter_tests/anime_season/season/year_only.html"))
-    
-        downloader.download("15890").writeToFile(resourceFile("file_converter_tests/anime_season/year_of_premiere/2021-08-06.html"))
-        downloader.download("5976").writeToFile(resourceFile("file_converter_tests/anime_season/year_of_premiere/1958-11.html"))
-        downloader.download("168").writeToFile(resourceFile("file_converter_tests/anime_season/year_of_premiere/1991.html"))
-        downloader.download("16275").writeToFile(resourceFile("file_converter_tests/anime_season/year_of_premiere/unknown.html"))
-    
-        downloader.download("6247").writeToFile(resourceFile("file_converter_tests/duration/1_hour.html"))
-        downloader.download("11051").writeToFile(resourceFile("file_converter_tests/duration/1_minute.html"))
-        downloader.download("6889").writeToFile(resourceFile("file_converter_tests/duration/2_hours.html"))
-        downloader.download("14844").writeToFile(resourceFile("file_converter_tests/duration/24_minutes_per_episode.html"))
-        downloader.download("7192").writeToFile(resourceFile("file_converter_tests/duration/63_minutes_by_6_episodes.html"))
-        downloader.download("7163").writeToFile(resourceFile("file_converter_tests/duration/70_minutes.html"))
-        downloader.download("602").writeToFile(resourceFile("file_converter_tests/duration/134_minutes.html"))
-        downloader.download("16711").writeToFile(resourceFile("file_converter_tests/duration/episodes_and_duration_unknown.html"))
-        downloader.download("17039").writeToFile(resourceFile("file_converter_tests/duration/episodes_known_duration_unknown.html"))
-    
-        downloader.download("9981").writeToFile(resourceFile("file_converter_tests/episodes/1.html"))
-        downloader.download("3135").writeToFile(resourceFile("file_converter_tests/episodes/10.html"))
-        downloader.download("4138").writeToFile(resourceFile("file_converter_tests/episodes/100.html"))
-        downloader.download("5801").writeToFile(resourceFile("file_converter_tests/episodes/1818.html"))
-        downloader.download("16804").writeToFile(resourceFile("file_converter_tests/episodes/type-is-double.html"))
-        downloader.download("16578").writeToFile(resourceFile("file_converter_tests/episodes/unknown.html"))
-    
-        downloader.download("15237").writeToFile(resourceFile("file_converter_tests/picture_and_thumbnail/neither_picture_nor_thumbnail.html"))
-        downloader.download("3633").writeToFile(resourceFile("file_converter_tests/picture_and_thumbnail/picture_and_thumbnail_available.html"))
-    
-        relationsDownloader.download("4942").writeToFile(resourceFile("file_converter_tests/related_anime/multiple_related_anime.html"))
-        downloader.download("4942").writeToFile(resourceFile("file_converter_tests/related_anime/multiple_related_anime_main.html"))
-        relationsDownloader.download("10941").writeToFile(resourceFile("file_converter_tests/related_anime/no_related_anime.html"))
-        downloader.download("10941").writeToFile(resourceFile("file_converter_tests/related_anime/no_related_anime_main.html"))
-        relationsDownloader.download("14844").writeToFile(resourceFile("file_converter_tests/related_anime/no_related_anime_but_adaption.html"))
-        downloader.download("14844").writeToFile(resourceFile("file_converter_tests/related_anime/no_related_anime_but_adaption_main.html"))
-        downloader.download("4942").writeToFile(resourceFile("file_converter_tests/related_anime/related_anime_file_missing_main.html"))
-        relationsDownloader.download("16777").writeToFile(resourceFile("file_converter_tests/related_anime/single_related_anime.html"))
-        downloader.download("16777").writeToFile(resourceFile("file_converter_tests/related_anime/single_related_anime_main.html"))
-    
-        downloader.download("3633").writeToFile(resourceFile("file_converter_tests/sources/3633.html"))
 
-        downloader.download("16925").writeToFile(resourceFile("file_converter_tests/status/on_hold.html"))
-        downloader.download("12433").writeToFile(resourceFile("file_converter_tests/status/aborted.html"))
-        downloader.download("3633").writeToFile(resourceFile("file_converter_tests/status/completed.html"))
-        downloader.download("6222").writeToFile(resourceFile("file_converter_tests/status/completed_in_japan_upcoming_elsewhere.html"))
-        downloader.download("14494").writeToFile(resourceFile("file_converter_tests/status/no_status.html"))
-        downloader.download("1721").writeToFile(resourceFile("file_converter_tests/status/ongoing.html"))
-        downloader.download("12224").writeToFile(resourceFile("file_converter_tests/status/upcoming.html"))
-    
-        downloader.download("1958").writeToFile(resourceFile("file_converter_tests/synonyms/multiple_synonyms.html"))
-        downloader.download("16260").writeToFile(resourceFile("file_converter_tests/synonyms/no_synonyms.html"))
-        downloader.download("13631").writeToFile(resourceFile("file_converter_tests/synonyms/romanji_alteration.html"))
-        downloader.download("14456").writeToFile(resourceFile("file_converter_tests/synonyms/single_synonym.html"))
-    
-        downloader.download("8724").writeToFile(resourceFile("file_converter_tests/synonyms/hidden_synonyms_8724.html"))
-        downloader.download("11197").writeToFile(resourceFile("file_converter_tests/synonyms/hidden_synonyms_11197.html"))
-        downloader.download("8093").writeToFile(resourceFile("file_converter_tests/synonyms/hidden_synonyms_and_named_parts.html"))
-        downloader.download("17015").writeToFile(resourceFile("file_converter_tests/synonyms/italic.html"))
-        downloader.download("1958").writeToFile(resourceFile("file_converter_tests/synonyms/multiple_synonyms.html"))
-        downloader.download("16260").writeToFile(resourceFile("file_converter_tests/synonyms/no_synonyms.html"))
-        downloader.download("13631").writeToFile(resourceFile("file_converter_tests/synonyms/romanji_alteration.html"))
-        downloader.download("14456").writeToFile(resourceFile("file_converter_tests/synonyms/single_synonym.html"))
-        downloader.download("15599").writeToFile(resourceFile("file_converter_tests/synonyms/synonyms_contain_named_parts.html"))
-    
-        downloader.download("15073").writeToFile(resourceFile("file_converter_tests/tags/multiple_tags.html"))
-        downloader.download("17467").writeToFile(resourceFile("file_converter_tests/tags/no_tags.html"))
-        downloader.download("613").writeToFile(resourceFile("file_converter_tests/tags/one_tag.html"))
-    
-        downloader.download("4410").writeToFile(resourceFile("file_converter_tests/title/title_not_set_in_jsonld.html"))
-        downloader.download("15159").writeToFile(resourceFile("file_converter_tests/title/special_chars.html"))
-    
-        downloader.download("10454").writeToFile(resourceFile("file_converter_tests/type/bonus.html"))
-        downloader.download("12290").writeToFile(resourceFile("file_converter_tests/type/cm.html"))
-        downloader.download("9981").writeToFile(resourceFile("file_converter_tests/type/movie.html"))
-        downloader.download("9830").writeToFile(resourceFile("file_converter_tests/type/music-video.html"))
-        downloader.download("16289").writeToFile(resourceFile("file_converter_tests/type/other.html"))
-        downloader.download("3627").writeToFile(resourceFile("file_converter_tests/type/ova.html"))
-        downloader.download("4946").writeToFile(resourceFile("file_converter_tests/type/tv-series.html"))
-        downloader.download("13250").writeToFile(resourceFile("file_converter_tests/type/tv-special.html"))
-        downloader.download("17467").writeToFile(resourceFile("file_converter_tests/type/unknown.html"))
-        downloader.download("14935").writeToFile(resourceFile("file_converter_tests/type/web.html"))
-
-        println("Done")
+    mainConfigFiles.forEach { (file, animeId) ->
+        downloader.download(animeId).writeToFile(resourceFile(file))
+        delay(random(5000, 10000))
     }
+
+    relationsConfigFiles.forEach { (file, animeId) ->
+        relationsDownloader.download(animeId).writeToFile(resourceFile(file))
+        delay(random(5000, 10000))
+    }
+
+    print("Done")
 }
 
 private fun resourceFile(file: String): Path {
@@ -114,4 +135,27 @@ private fun resourceFile(file: String): Path {
             .toString()
             .replace("/build/resources/test/", "/src/test/resources/")
     )
+}
+
+internal class UpdateTestResourcesTest {
+
+    @Test
+    fun `verify that all test resources a part of the update sequence`() {
+        // given
+        val testResourcesFolder = "file_converter_tests"
+
+        val filesInTestResources = Files.walk(testResource(testResourcesFolder))
+            .filter { it.isRegularFile() }
+            .filter { it.fileSuffix() == AnisearchConfig.fileSuffix() }
+            .map { it.toString() }
+            .toList()
+
+        // when
+        val filesInList = mainConfigFiles.keys.union(relationsConfigFiles.keys).map {
+            it.replace(testResourcesFolder, testResource(testResourcesFolder).toString())
+        }
+
+        // then
+        assertThat(filesInTestResources.sorted()).isEqualTo(filesInList.sorted())
+    }
 }
